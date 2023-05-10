ed58afba703c9b610d11083799b48b00fc42ef60
Gabor Lizik <glizik@gmail.com>
2023-05-10 15:11:36 +0200

    init

---
+++ CleanArchitectureComposition/CleanArchitectureComposition/CleanArchitectureCompositionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitectureCompositionApp.swift
+//  CleanArchitectureComposition
+//
+//  Created by Mac on 2023. 05. 09..
+//
+
+import SwiftUI
+
+@main
+struct CleanArchitectureCompositionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
